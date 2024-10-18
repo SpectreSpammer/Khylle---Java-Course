cb3b7aee6ba4a17a8303706444b841eeddde3dc7
SpectreSpammer <rnavarra.business@gmail.com>
2024-10-19 03:03:33 +0800

    Day 1 commit message

--- src/main/java/day2_java_syntax_basics/MainJavaBasics.java
+++ src/main/java/day2_java_syntax_basics/MainJavaBasics.java
@@ -3,5 +3,6 @@ package day2_java_syntax_basics;
 public class MainJavaBasics {
     public static void main(String[] args) {
         System.out.println("Welcome Khylle");
+        System.out.println("Java Backend");
     }
 }
