7dde3e6486bcaf51ae556e4fa413c342592f9334
SpectreSpammer <rnavarra.business@gmail.com>
2024-10-31 18:02:27 +0800

    Oct 31- Day 2 Accomplishments → Day 2 Java Syntax Basics

--- src/main/java/day2_java_syntax_basics/MainJavaBasics.java
+++ src/main/java/day2_java_syntax_basics/MainJavaBasics.java
@@ -4,5 +4,30 @@ public class MainJavaBasics {
     public static void main(String[] args) {
         System.out.println("Welcome Khylle");
         System.out.println("Java Backend");
+
+        sayMessage();
+
+        //calling a method with a no return value
+        Message message = new Message();
+        message.sayOtherMessage();
+        System.out.println();
+
+        //for calling a static method
+        Message.anotherMessage();
+
+        //This is a one line comment
+        /*
+            This
+            is
+            a
+            multi
+            line
+
+         */
+    }
+
+    private static void sayMessage() {
+        System.out.println("This is a sayMethod() method");
     }
 }
+
---
+++ src/main/java/day2_java_syntax_basics/Message.java
@@ -0,0 +1,12 @@
+package day2_java_syntax_basics;
+
+public class Message {
+
+    public void sayOtherMessage(){
+        System.out.println("This is from another class");
+    }
+
+    public static void anotherMessage(){
+        System.out.println("This is a static method message");
+    }
+}
